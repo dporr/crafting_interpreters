7781567dae2a98a65b68d9259aaf0a02dd52b866
Diego Porras <diego.porras@owasp.org>
2023-06-20 10:32:30 +0100

    Add Doubly-linked-list operations

--- DoubleLinkedList.c
+++ DoubleLinkedList.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+
+#define FIELD_SIZE 30
 
 typedef struct node {
 	struct node* prev;
@@ -8,17 +13,22 @@ typedef struct node {
 } node_t;//Double linked list
 
 
+node_t *HEAD;
+
 int main(int argc,char* argv[]){
 	node_t *first_node = (node_t*)(malloc(sizeof(node_t)));
 	node_t *second_node = (node_t*)(malloc(sizeof(node_t)));
+	HEAD = first_node;
 	first_node->prev = NULL;
 	first_node->next = second_node;
-	first_node->value = malloc(30);
+	first_node->value = malloc(FIELD_SIZE);
 	
 	second_node->prev =  first_node;
 	second_node->next = NULL;
-	second_node->value = malloc(30);
-
+	second_node->value = malloc(FIELD_SIZE);
+	strncpy(first_node->value, "Im the first", FIELD_SIZE);
+	strncpy(second_node->value, "Im the SECOND", FIELD_SIZE);
+	find("AA");
 	free(first_node);
 	free(second_node);
 	return 0;
@@ -41,3 +51,21 @@ int insert(node_t  *current, node_t *new){
 	}
 }
 
+//Transverse -> derreference value -> compare with searched value
+int find(char *value){
+	node_t *current = HEAD;
+	while(current){
+		if(strcmp(current->value, value) == 0){
+			return 1;
+		}
+		current = current->next;
+	}
+	return -1;
+}
+int delete(node_t *node){
+	if(node == NULL) return -1;
+	node->next->prev = node->prev;
+	node->prev->next = node->next;
+	free(node);
+	return 1;
+}
